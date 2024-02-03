6dac5658cf5db9df8e1f4f8a634c7cc1347b42d2
Anurag7645 <114188019+Anurag7645@users.noreply.github.com>
2024-02-04 01:16:43 +0530

    change

--- JavaPrograms lab/Scannertest.java
+++ JavaPrograms lab/Scannertest.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Main {
+public class Scannertest {
   public static void main(String[] args) {
     // creates an object of Scanner
     Scanner input = new Scanner(System.in);
